8741aa11dee8e0eed97d51063199b283667470b0
Simon Jefferies <simon@olivegame.studio>
2024-08-19 21:11:03 +0100

    Add guard class.

---
+++ Utility.Guard/Guard.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+
+namespace Utility;
+
+public static class Guard
+{
+    public static T ThrowIfNull<T>(T item)
+    {
+        if (item is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return item;
+    }
+
+    public static T ThrowIfNotNull<T>(T item)
+    {
+        if (item is not null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return item;
+    }
+
+    public static int ThrowIfIntegerNotInRange(int item, int start, int end)
+    {
+        if (item >= start && item <= end)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return item;
+    }
+
+    public static string ThrowIfStringNullOrEmpty(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            throw new InvalidOperationException();
+        }
+
+        return value;
+    }
+}
